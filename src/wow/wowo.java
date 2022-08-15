e76c2de2f3bef3bed06609d60fb01a889938c402
rakibbiswas1 <rakibbiswas.official@gmail.com>
2022-08-15 23:44:25 +0530

    checking user input

---
+++ src/wow/wowo.java
@@ -0,0 +1,14 @@
+package wow;
+import java.util.Scanner;
+public class wowo {
+	public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       System.out.print("Enter 1st number: ");
+       float a = sc.nextFloat();
+       System.out.print("enter 2nd number: ");
+       float b = sc.nextFloat();
+       float sum = a+b;
+       System.out.println(sum);
+ }
+}
+
