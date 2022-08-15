16892b5440fe42411f8438e5ead9f5f82854f559
rakibbiswas1 <rakibbiswas.official@gmail.com>
2022-08-16 01:11:55 +0530

    checking userinput and sum

--- src/wow/wowo.java
+++ src/wow/wowo.java
@@ -1,21 +1,13 @@
 package wow;
 import java.util.Scanner;
-
 public class wowo {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter 1st number: ");
-        int a = sc.nextInt();
-        System.out.println("Enter 2nd number: ");
-        int b = sc.nextInt();
-        if (a<b){
-            System.out.println("if block executed");
-        } else if (a>b) {
-        	System.out.println("if else block executed");
-        }
-        else {
-            System.out.println("else block executed");
-        }
-    }
+	public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       System.out.print("Enter 1st number: ");
+       float a = sc.nextFloat();
+       System.out.print("enter 2nd number: ");
+       float b = sc.nextFloat();
+       float sum = a+b;
+       System.out.println(sum);
+ }
 }
